94f728ecacd88ce910bd176df5462170a0f24778
unknown <gwanjuchung@yahoo.com>
2022-11-19 20:57:29 -0800

    Put in empty get and post routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,13 @@
 const notes = require('express').Router();
+const app = require('.');
+const db = require('../db/db.json');
+
+app.get('/api/notes', (req, res) => {
+
+});
+
+app.post('/api/nots', (req, res) => {
+
+});
 
 module.exports = notes;
