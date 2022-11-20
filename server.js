5538c48c16a8cb5b8897d5f64b9b3e6905d1f638
unknown <gwanjuchung@yahoo.com>
2022-11-19 23:08:47 -0800

    Pulled in and fixed routes into the server.js file

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path  = require('path');
-const api = require('./routes/index.js');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -21,6 +22,48 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for feedback`);
+
+  return res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit feedback`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    // push the new note to the array
+    notes.push(newNote);
+    let newNotes = JSON.stringify(notes);
+
+    // write file to the db folder
+    fs.writeFile(`./db/db.json`, newNotes, err => {
+        err ? console.error(err) : console.log('Success!')
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
